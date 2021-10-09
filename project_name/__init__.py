22fcec6fc02fbd7aa74bb632fe94c810b5bad736
William Abreu <william@t10.digital>
2021-10-09 19:54:45 -0300

    Dropped settings file, using __init__ instead

--- project_name/__init__.py
+++ project_name/__init__.py
@@ -1,3 +1,7 @@
+# ==========================
+# Constants for the project.
+# ==========================
+
 # Define the package's version.
 # - Is used for indexing and distributing the package
 # - Follow the conventions defined by PEP440
